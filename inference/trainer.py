be9f57541974b91c524c03454172ec695d93bc05
tienndm <tien.nguyen@altek.com.vn>
2025-02-13 14:04:10 +0700

    Revert "feat: update loss"
    This reverts commit 7677e07aaad112642d368cc9bc93f9e26800f71d.

--- inference/trainer.py
+++ inference/trainer.py
@@ -75,10 +75,10 @@ class Trainer:
         self.optimizer = Adam(self.model.parameters(), lr=lr)
         if self.schedule:
             self.scheduler = StepLR(self.optimizer, step_size=10, gamma=0.1)
-        self.criterion = LabelSmoothingLoss(
-            classes=len(self.dataset.vocab),
-            smoothing=0.1,
+        
+        self.criterion = torch.nn.CrossEntropyLoss(
             ignore_index=self.dataset.vocab.stoi["<PAD>"],
+            label_smoothing=0.1,
         )
         self.best_val_loss = float("inf")
         if self.mixedPrecision:
